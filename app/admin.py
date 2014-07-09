e5821d02545622a6a1851d384c8c42fb28b8f8b0
doyomay <cgme.may@gmail.com>
2014-07-09 14:05:18 -0500

    Agregar funciones al admin de django
    agregado list_display, list_filter, search_fields, mostrar funciones

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from models import *
 
 # Register your models here.
-
+class EnlaceAdmin(admin.ModelAdmin):
+	list_display = ('titulo','enlace','categoria','votos','imagen_voto')
+	list_filter = ('categoria','usuario','timestamp',)
+	search_fields = ('categoria__titulo','enlace__titulo','usuario__username')
+	def imagen_voto(self, obj):
+		url = obj.mis_votos_en_imagen_rosada()
+		tag = '<img src="%s">' % url
+		return tag
+	imagen_voto.allow_tags = True
 admin.site.register(Categoria)
-admin.site.register(Enlace)
+admin.site.register(Enlace,EnlaceAdmin)
--- app/models.py
+++ app/models.py
@@ -17,3 +17,5 @@ class Enlace(models.Model):
 	timestamp = models.DateTimeField(auto_now_add = True)
 	def __unicode__(self):
 		return "%s -- %s " % (self.titulo,self.enlace)
+	def mis_votos_en_imagen_rosada(self):
+		return 'http://placehold.it/100x100&text=%d+votos'% self.votos
